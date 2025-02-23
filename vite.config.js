de09dc6dbb2a539fd7f48b1b3ea73f7b8efd5fa9
Jana1902 <jana7cr@gmail.com>
2025-02-23 23:18:58 +0530

    light change

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import { defineConfig } from "vite";
 export default defineConfig({
   root: "public", // Use the "public" folder as the project root
   build: {
+    rollupOptions: {
+      external: ["jquery"],
+    },
     outDir: "../dist", // Output the built files into a "dist" folder at the project root
     emptyOutDir: true,
   },
