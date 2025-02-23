2d89a51f27cc6e6550cc6c23dc63b52695022df2
Jana1902 <jana7cr@gmail.com>
2025-02-23 22:38:40 +0530

    Lastt

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,10 @@
-// vite.config.js
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  resolve: {
-    alias: {
-      // Adjust the path if necessary. This example assumes that the file is at node_modules/stockfish/stockfish.js
-      stockfish: "stockfish/stockfish.js",
-    },
-  },
-  optimizeDeps: {
-    exclude: ["stockfish"],
-  },
-  base: "./", // Ensures relative paths work
-  publicDir: "public", // Ensures assets like Stockfish.js are served
+  base: './',
+  publicDir: 'public',
   build: {
-    outDir: "dist",
-    assetsDir: "assets",
-  },
+    outDir: 'dist',
+    assetsDir: 'assets'
+  }
 });
